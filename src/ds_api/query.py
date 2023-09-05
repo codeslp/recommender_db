ba39e5d0070fb025060b71b9c821c1ebd66fcd32
Brian Farish <brian.farish@gmail.com>
2023-09-05 14:39:45 -0500

    added instructions

--- src/ds_api/query.py
+++ src/ds_api/query.py
@@ -1,8 +1,24 @@
 from db_api import read, write
 
 """
-You may pass the parameter verbose=False to query_db() to suppress the output.
-Right now ds_user does not have permission to create alter table structure.
+Instructions for using this module:
+
+1. Use the `read` function to execute SELECT queries.
+   - The result will be printed in a table format by default.
+   - To suppress the table output, pass `verbose=False` as an argument.
+
+2. Use the `write` function for executing INSERT, UPDATE, DELETE, or other non-SELECT SQL statements.
+
+3. Note: The current user 'ds_user' does not have permissions to modify the table structure (e.g., ALTER TABLE). 
+   Ensure you have the necessary permissions or consult the database administrator before attempting structural changes.
+
+Example usage:
+
+query = "INSERT INTO users (user_id, ...) VALUES (...);"
+write(query)
+
+data_frame = read("SELECT * FROM users WHERE ...;")
+
 """
 
 query = """
@@ -10,7 +26,6 @@ query = """
     VALUES ('999', '2020-01-01', '2021-01-01', 'premium');
     """
 write(query)
-
 
 read("""
      SELECT * 
