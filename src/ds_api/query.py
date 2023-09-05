a015bb45df6c9d6b7f16a5aa0b9178e9c6c88345
Brian Farish <brian.farish@gmail.com>
2023-09-05 14:33:09 -0500

    separated read and write operations

--- src/ds_api/query.py
+++ src/ds_api/query.py
@@ -1,7 +1,24 @@
-from db_api import query_db
+from db_api import read, write
 
 """
 You may pass the parameter verbose=False to query_db() to suppress the output.
+Right now ds_user does not have permission to create alter table structure.
 """
 
-query_db("SELECT * FROM relational.users;")
+query = """
+    INSERT INTO users (user_id, birth_date, subscription_date, subscription_type) 
+    VALUES ('999', '2020-01-01', '2021-01-01', 'premium');
+    """
+write(query)
+
+
+read("""
+     SELECT * 
+     FROM users
+     WHERE user_id IN ('999');
+     """)
+
+query = """
+    DELETE FROM users WHERE user_id = '999';
+    """
+write(query)
